66c401c9160e78da2ce7176f93c805d5886923a1
Hyeonseung Kang <h5k@ajou.ac.kr>
2022-09-25 23:23:11 +0900

    export

---
+++ lib/printer/printer.d.ts
@@ -0,0 +1,38 @@
+/// <reference types="node" />
+/// <reference types="node" />
+import { ParsedBodyInterface } from '../server/open-server';
+import { TypedEmitter } from 'tiny-typed-emitter';
+interface PrinterEvents {
+    serverStart: (err: Error | null) => void;
+    bonjourPublish: () => void;
+    operations: (requestBody: ParsedBodyInterface, responseBody: any) => void;
+    printJobOperation: (requestBody: ParsedBodyInterface, responseBody: any) => void;
+    raw: (requestBuffer: Buffer) => void;
+}
+interface PrinterOption {
+    name?: string;
+    port?: number;
+    host?: string;
+    additional?: {
+        description?: string;
+        location?: string;
+        makeAndModel?: string;
+        moreInfo?: string;
+    };
+}
+export declare class HandledJob {
+    constructor(handledJobs: HandledJob[], jobName: string, jobOriginatingUserName: string);
+    readonly 'job-id': number;
+    readonly 'job-state' = 9;
+    readonly 'job-name': string;
+    readonly 'job-originating-user-name': string;
+    readonly createdAt: number;
+}
+export declare class Printer extends TypedEmitter<PrinterEvents> {
+    constructor(printerOption?: PrinterOption);
+    readonly printerOption: PrinterOption;
+    readonly handledJobs: HandledJob[];
+    readonly started: number;
+    readonly server: import("fastify").FastifyInstance<import("http").Server<typeof import("http").IncomingMessage, typeof import("http").ServerResponse>, import("http").IncomingMessage, import("http").ServerResponse<import("http").IncomingMessage>, import("fastify").FastifyBaseLogger, import("fastify").FastifyTypeProviderDefault> & PromiseLike<import("fastify").FastifyInstance<import("http").Server<typeof import("http").IncomingMessage, typeof import("http").ServerResponse>, import("http").IncomingMessage, import("http").ServerResponse<import("http").IncomingMessage>, import("fastify").FastifyBaseLogger, import("fastify").FastifyTypeProviderDefault>>;
+}
+export {};
